81ab7c486088828c4a5d2034e67be4102a8f9ccf
md-arif-islam <arifislamarif344@gmail.com>
2023-06-08 20:08:35 +0600

    Updated Version

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const submitButton = document.getElementById("submit");
-const passwordValidationText = document.getElementById(
-  "password-validation-text"
-);
+$(document).ready(function () {
+  const passwordInput = $("#password");
+  const submitButton = $("#submit");
+  const passwordValidationText = $("#password-validation-text");
 
-submitButton.addEventListener("mouseover", (button) => {
-  let password = passwordInput.value;
-  let email = emailInput.value;
-
+  let password = passwordInput.val();
   let validate = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8}$/.test(password);
 
-  if (!validate) {
-    button.target.classList.toggle("move");
-    submitButton.style.background = "red";
-    passwordValidationText.textContent =
-      "Please use letters and numbers to make your password stronger.";
-  } else {
-    button.target.classList.add("stop");
-    submitButton.style.background = "green";
-    passwordValidationText.textContent = ""; // Clear the validation text when the password is valid
-  }
+  passwordInput.on("input", function () {
+    if (!validate) {
+      submitButton.css("background", "red");
+      passwordValidationText.text(
+        "Please use letters and numbers to make your password stronger."
+      );
+    } else {
+      submitButton.css("background", "green");
+      passwordValidationText.text("Awesome");
+    }
+  });
+
+  submitButton.on("mouseover", function () {
+    if (!validate) {
+      submitButton.toggleClass("move");
+      submitButton.css("background", "red");
+    } else {
+      submitButton.addClass("stop");
+      submitButton.css("background", "green");
+    }
+  });
 });
